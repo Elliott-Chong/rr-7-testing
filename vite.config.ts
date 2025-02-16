d64657187c365ae20cb0a30891d72ddd89793fae
Elliott Chong <77007117+Elliott-Chong@users.noreply.github.com>
2025-02-16 22:17:23 +0800

    try

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { barrel } from 'vite-plugin-barrel'
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { defineConfig } from "vite";
@@ -6,7 +7,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: { port: 3000, strictPort: true },
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    barrel({ packages: ['lucide-react'] }),
+  ],
   define: {
     "process.env": process.env,
   },
